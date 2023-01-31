1033142e347195c8b4fa14724432950de68fed84
DashaErmolich <dasha.ermolich@gmail.com>
2023-01-31 15:58:07 +0100

    test: api

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export default class AppComponent {
+export default class AppComponent implements OnInit {
   title = 'music-app';
+
+  ngOnInit() {
+    this.getTrack();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async getTrack(): Promise<void> {
+    const response = await fetch('https://api.deezer.com/album/302128%27');
+    const result = await response.json();
+    // eslint-disable-next-line no-console
+    console.log(result);
+  }
 }
