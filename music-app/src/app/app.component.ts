3e7a8aae2517021bf373a0d9583471b9975d7bd0
DashaErmolich <dasha.ermolich@gmail.com>
2023-01-31 18:42:31 +0100

    test: fetch test

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -2,27 +2,27 @@
 declare var require: any;
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
+import { Subscription, catchError } from 'rxjs';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const DeezerPublicApi = require('deezer-public-api');
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// const DeezerPublicApi = require('deezer-public-api');
 
-const deezer = new DeezerPublicApi();
+// const deezer = new DeezerPublicApi();
 
-// Search an artist
-deezer.search.artist('ILYSH').then((result: any) => {
-  console.log(result);
-});
+// // Search an artist
+// deezer.search.artist('ILYSH').then((result: any) => {
+//   console.log(result);
+// });
 
-// Get info for the given artist id
-deezer.artist('58671252').then((result: any) => {
-  console.log(result);
-});
+// // Get info for the given artist id
+// deezer.artist('58671252').then((result: any) => {
+//   console.log(result);
+// });
 
-// Get album list for the given artist id
-deezer.artist.albums('58671252').then((result: any) => {
-  console.log(result);
-});
+// // Get album list for the given artist id
+// deezer.artist.albums('58671252').then((result: any) => {
+//   console.log(result);
+// });
 
 @Component({
   selector: 'app-root',
@@ -37,26 +37,63 @@ export default class AppComponent {
   constructor(private http: HttpClient) {
   }
 
+  // // handleError(str: string, arr: []) {
+  // //   console.log(str);
+  // // }
+
+  // ngOnInit() {
+  //   const response = this.http.get('https://api.deezer.com/album/302128%27', {
+  //     responseType: "jsonp",
+  //   })
+  // }
+
   // ngOnInit(): void {
-  //   const response = this.http.get('https://api.deezer.com/album/302128%27');
+  //   const response = this.http.jsonp('https://api.deezer.com/album/302128%27', 'callback').pipe(
+  //     catchError(this.handleError('searchHeroes', [])) // then handle the error
+  //   );
   //   this.sub = response.subscribe((x) => {
   //     console.log(x);
   //   });
+
+  //   // console.log(this.http.jsonp('https://api.deezer.com/album/302128%27'))
+  // }
+
+  // handleError(arg0: string, arg1: never[]): (err: any, caught: import("rxjs").Observable<Object>) => import("rxjs").ObservableInput<any> {
+  //   throw new Error('Method not implemented.');
   // }
 
   // ngOnDestroy() {
   //   this.sub.unsubscribe();
   // }
 
-  // ngOnInit() {
-  //   this.getTrack();
-  // }
+  ngOnInit() {
+    this.getTrack();
+  }
 
   // // eslint-disable-next-line class-methods-use-this
-  // async getTrack(): Promise<void> {
-  //   const response = await fetch('https://api.deezer.com/album/302128%27');
-  //   const result = await response.json();
-  //   // eslint-disable-next-line no-console
-  //   console.log(result);
+  async getTrack(): Promise<void> {
+    const cors = "https://cors-anywhere.herokuapp.com/";
+    const url = 'https://api.deezer.com/album/302128%27'
+    const response = await fetch(cors + url, {
+      headers: {
+        'Content-Type': 'application/jsonp'
+      }
+    });
+    const result = await response.json();
+    // eslint-disable-next-line no-console
+    console.log(result);
+  }
+
+  // name:string;
+  // res: any;
+  // constructor(http: HttpClient) {
+  //   this.name = `Angular!`;
+
+  //   const params = {
+  //     output: 'jsonp',
+  //   }
+
+  //   http.jsonp('https://api.stackexchange.com/2.2/info?site=stackoverflow', 'callback')
+  //     .subscribe(res => console.log(res));
   // }
 }
--- music-app/src/app/app.module.ts
+++ music-app/src/app/app.module.ts
@@ -1,20 +1,39 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+// import { JsonpModule, Jsonp, Response } from '@angular/common/http';
+
+// import AppComponent from './app.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import AppRoutingModule from './app-routing.module';
+
 import AppComponent from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpClientJsonpModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export default class AppModule { }
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HttpClientModule,
+//     HttpClientJsonpModule,
+//     JsonpModule,
+//     Jsonp,
+//     Response
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export default class AppModule { }
