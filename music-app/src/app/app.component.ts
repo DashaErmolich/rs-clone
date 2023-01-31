f0887b10ed32458cdc5736e10daac2c1842d91a9
DashaErmolich <dasha.ermolich@gmail.com>
2023-01-31 22:55:00 +0100

    feat: add new cors proxy url

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -1,25 +1,26 @@
-/* eslint-disable */
-declare var require: any;
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subscription, catchError } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export default class AppComponent {
+export default class AppComponent implements OnInit, OnDestroy {
   title = 'music-app';
+
   sub = new Subscription();
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    const cors = "https://cors-anywhere.herokuapp.com/";
+    const cors = 'http://localhost:8080/';
     const url = 'https://api.deezer.com/track/3135556';
     const response = this.http.get(cors + url);
     this.sub = response.subscribe((x) => {
+      // eslint-disable-next-line no-console
       console.log(x);
     });
   }
