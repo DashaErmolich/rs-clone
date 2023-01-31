6596a722714e18a9bcef0cb416a4040669c6394d
DashaErmolich <dasha.ermolich@gmail.com>
2023-01-31 16:18:15 +0100

    test: api

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable no-console */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export default class AppComponent implements OnInit {
+export default class AppComponent implements OnInit, OnDestroy {
   title = 'music-app';
 
-  ngOnInit() {
-    this.getTrack();
+  sub = new Subscription();
+
+  constructor(private http: HttpClient) {
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  async getTrack(): Promise<void> {
-    const response = await fetch('https://api.deezer.com/album/302128%27');
-    const result = await response.json();
-    // eslint-disable-next-line no-console
-    console.log(result);
+  ngOnInit(): void {
+    const response = this.http.get('https://api.deezer.com/album/302128%27');
+    this.sub = response.subscribe((x) => {
+      console.log(x);
+    });
   }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  // ngOnInit() {
+  //   this.getTrack();
+  // }
+
+  // // eslint-disable-next-line class-methods-use-this
+  // async getTrack(): Promise<void> {
+  //   const response = await fetch('https://api.deezer.com/album/302128%27');
+  //   const result = await response.json();
+  //   // eslint-disable-next-line no-console
+  //   console.log(result);
+  // }
 }
