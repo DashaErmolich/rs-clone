c8366f957646b485f773c72b246b7f4ea7208f40
DashaErmolich <dasha.ermolich@gmail.com>
2023-01-31 16:39:34 +0100

    test: test api

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -1,14 +1,35 @@
-/* eslint-disable no-console */
+/* eslint-disable */
+declare var require: any;
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const DeezerPublicApi = require('deezer-public-api');
+
+const deezer = new DeezerPublicApi();
+
+// Search an artist
+deezer.search.artist('ILYSH').then((result: any) => {
+  console.log(result);
+});
+
+// Get info for the given artist id
+deezer.artist('58671252').then((result: any) => {
+  console.log(result);
+});
+
+// Get album list for the given artist id
+deezer.artist.albums('58671252').then((result: any) => {
+  console.log(result);
+});
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export default class AppComponent implements OnInit, OnDestroy {
+export default class AppComponent {
   title = 'music-app';
 
   sub = new Subscription();
@@ -16,16 +37,16 @@ export default class AppComponent implements OnInit, OnDestroy {
   constructor(private http: HttpClient) {
   }
 
-  ngOnInit(): void {
-    const response = this.http.get('https://api.deezer.com/album/302128%27');
-    this.sub = response.subscribe((x) => {
-      console.log(x);
-    });
-  }
+  // ngOnInit(): void {
+  //   const response = this.http.get('https://api.deezer.com/album/302128%27');
+  //   this.sub = response.subscribe((x) => {
+  //     console.log(x);
+  //   });
+  // }
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
+  // ngOnDestroy() {
+  //   this.sub.unsubscribe();
+  // }
 
   // ngOnInit() {
   //   this.getTrack();
