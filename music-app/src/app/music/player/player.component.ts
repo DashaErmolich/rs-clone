05abfedff06dfaed316f3aaec969849da1d71fa4
DashaErmolich <dasha.ermolich@gmail.com>
2023-02-09 17:02:16 +0100

    feat: like track button

--- music-app/src/app/music/player/player.component.ts
+++ music-app/src/app/music/player/player.component.ts
@@ -1,3 +1,113 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { AudioService } from '../../services/audio.service';
+// import { ITrackResponse } from '../../models/api-response.models';
+// import { DeezerRestApiService } from '../../services/deezer-api.service';
+
+// @Component({
+//   selector: 'app-player',
+//   templateUrl: './player.component.html',
+//   styleUrls: ['./player.component.scss'],
+// })
+// export class PlayerComponent implements OnInit {
+//   isLiked!: Observable<boolean>;
+
+//   isFirstTrack!: boolean;
+
+//   isPlay!: boolean;
+
+//   isLastTrack!: boolean;
+
+//   currentTime!: string;
+
+//   durationTime!: string;
+
+//   maxTimeValue!: number;
+
+//   currentProgress!: number;
+
+//   isMute!: boolean;
+
+//   isRepeatAllOn!: boolean;
+
+//   isRepeatOneOn!: boolean;
+
+//   currentVolume!: number;
+
+//   tracks!: Partial<ITrackResponse>[];
+
+//   constructor(
+//     private audioService: AudioService,
+//     private deezerRestApiService: DeezerRestApiService,
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.isFirstTrack = this.audioService.isFirstTrack;
+//     this.isPlay = this.audioService.isPlay;
+//     this.isLastTrack = this.audioService.isLastTrack;
+//     this.currentTime = this.audioService.currentTime;
+//     this.durationTime = this.audioService.durationTime;
+//     this.maxTimeValue = this.audioService.maxTimeValue;
+//     this.currentProgress = this.audioService.currentProgress;
+//     this.isMute = this.audioService.isMute;
+//     this.isRepeatAllOn = this.audioService.isRepeatAllOn;
+//     this.isRepeatOneOn = this.audioService.isRepeatOneOn;
+//     this.currentVolume = this.audioService.currentVolume;
+//     this.deezerRestApiService.getSearch('queen', 0, 5).subscribe((response) => {
+//       this.tracks = response.data;
+//     });
+//   }
+
+//   playTrack(url: string | undefined) {
+//     this.audioService.tracks = this.tracks;
+//     this.audioService.playTrack(url);
+//   }
+
+//   playPause() {
+//     this.audioService.playPause();
+//   }
+
+//   setVolume(event: Event) {
+//     this.audioService.setVolume(event);
+//   }
+
+//   toggleMute(): void {
+//     this.audioService.toggleMute();
+//   }
+
+//   setProgress(event: Event) {
+//     this.audioService.setProgress(event);
+//   }
+
+//   setTrackIndex(index: number) {
+//     this.audioService.setTrackIndex(index);
+//   }
+
+//   next() {
+//     this.audioService.next();
+//   }
+
+//   prev() {
+//     this.audioService.prev();
+//   }
+
+//   toggleRepeat() {
+//     this.audioService.toggleRepeat();
+//   }
+
+//   toggleRepeatOne() {
+//     this.audioService.toggleRepeatOne();
+//   }
+
+//   likeTrack() {
+//     this.audioService.likeTrack();
+//   }
+
+//   getValue() {
+//     return this.audioService.isLiked$;
+//   }
+// }
+
 /* eslint-disable max-len */
 /* eslint-disable no-debugger */
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -46,13 +156,17 @@ export class PlayerComponent implements OnInit {
 
   isMute = false;
 
-  isRepeatOn = false;
+  isRepeatAllOn = false;
+
+  isRepeatOneOn = false;
 
   currentTrackIndex: number | null = null;
 
   isFirstTrack = false;
 
   isLastTrack = false;
+
+  isLiked = false;
 
   audio = new Audio();
 
@@ -68,6 +182,10 @@ export class PlayerComponent implements OnInit {
   }
 
   playTrack(url: string | undefined) {
+    // eslint-disable-next-line no-debugger
+    // debugger;
+    // eslint-disable-next-line no-console
+    console.log(this.tracks);
     this.state$.next(this.defaultState);
     this.isPlay = true;
 
@@ -87,8 +205,14 @@ export class PlayerComponent implements OnInit {
       });
 
       this.audio.addEventListener('ended', () => {
-        this.state$.next(this.defaultState);
-        this.isPlay = false;
+        if (this.isRepeatOneOn && this.currentTrackIndex !== null) {
+          this.playTrack(this.tracks[this.currentTrackIndex].preview);
+        } else if (!this.isRepeatAllOn && this.isLastTrack) {
+          this.state$.next(this.defaultState);
+          this.isPlay = false;
+        } else {
+          this.next();
+        }
       });
     }
   }
@@ -141,6 +265,7 @@ export class PlayerComponent implements OnInit {
     }
   }
 
+  // eslint-disable-next-line class-methods-use-this
   getTime(sec: number, format: string = 'mm:ss') {
     return moment.utc(sec * 1000).format(format);
   }
@@ -155,10 +280,10 @@ export class PlayerComponent implements OnInit {
 
   setTrackIndex(index: number) {
     this.currentTrackIndex = index;
-    if (this.currentTrackIndex === 0 && !this.isRepeatOn) {
+    if (this.currentTrackIndex === 0 && !this.isRepeatAllOn) {
       this.isFirstTrack = true;
       this.isLastTrack = false;
-    } else if (this.currentTrackIndex === this.tracks.length - 1 && !this.isRepeatOn) {
+    } else if (this.currentTrackIndex === this.tracks.length - 1 && !this.isRepeatAllOn) {
       this.isLastTrack = true;
       this.isFirstTrack = false;
     } else {
@@ -171,7 +296,7 @@ export class PlayerComponent implements OnInit {
     if (this.currentTrackIndex !== null) {
       this.currentTrackIndex += 1;
       if (this.currentTrackIndex >= this.tracks.length) {
-        if (this.isRepeatOn) {
+        if (this.isRepeatAllOn) {
           this.currentTrackIndex = 0;
         } else {
           this.isLastTrack = true;
@@ -183,11 +308,10 @@ export class PlayerComponent implements OnInit {
   }
 
   prev() {
-    console.log(this.currentTrackIndex);
     if (this.currentTrackIndex !== null) {
       this.currentTrackIndex -= 1;
       if (this.currentTrackIndex < 0) {
-        if (this.isRepeatOn) {
+        if (this.isRepeatAllOn) {
           this.currentTrackIndex = this.tracks.length - 1;
         } else {
           this.isFirstTrack = true;
@@ -199,8 +323,9 @@ export class PlayerComponent implements OnInit {
   }
 
   toggleRepeat() {
-    this.isRepeatOn = !this.isRepeatOn;
-    if (this.isRepeatOn) {
+    this.isRepeatAllOn = !this.isRepeatAllOn;
+    this.isRepeatOneOn = false;
+    if (this.isRepeatAllOn) {
       this.isFirstTrack = false;
       this.isLastTrack = false;
     } else if (this.currentTrackIndex === 0) {
@@ -213,5 +338,14 @@ export class PlayerComponent implements OnInit {
       this.isFirstTrack = false;
       this.isLastTrack = false;
     }
+  }
+
+  toggleRepeatOne() {
+    this.isRepeatAllOn = false;
+    this.isRepeatOneOn = !this.isRepeatOneOn;
+  }
+
+  likeTrack() {
+    this.isLiked = !this.isLiked;
   }
 }
