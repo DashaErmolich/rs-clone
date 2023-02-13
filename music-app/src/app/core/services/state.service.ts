26d93a4f1ed2788a7c43bd913c28e223f36cb450
dasha.ermolich <93545289+DashaErmolich@users.noreply.github.com>
2023-02-13 17:31:53 +0100

    fix: default track index

--- music-app/src/app/core/services/state.service.ts
+++ music-app/src/app/core/services/state.service.ts
@@ -10,7 +10,7 @@ import { LocalStorageService } from './local-storage.service';
 export class StateService {
   trackList$ = new BehaviorSubject<Partial<ITrackResponse>[]>([]);
 
-  playingTrackIndex$ = new BehaviorSubject<number | null>(0);
+  playingTrackIndex$ = new BehaviorSubject<number | null>(null);
 
   constructor(private storage: LocalStorageService) {
     const trackListInfo = this.storage.getTrackListInfo();
