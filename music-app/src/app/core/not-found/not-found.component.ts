f8afc10ee8fa1e2b0af8f15da5cca8b30eb8b398
DashaErmolich <dasha.ermolich@gmail.com>
2023-02-15 23:05:28 +0100

    feat: add not found page

--- music-app/src/app/core/not-found/not-found.component.ts
+++ music-app/src/app/core/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
+  constructor(
+    private myTheme: ThemeService,
+  ) { }
 
+  theme: string = this.myTheme.activeTheme;
 }
