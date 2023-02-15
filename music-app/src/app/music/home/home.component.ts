c6a64d0e3c743e4b443f67d10845ec5a8538fd72
DashaErmolich <dasha.ermolich@gmail.com>
2023-02-15 16:39:15 +0100

    feat: greeting message

--- music-app/src/app/music/home/home.component.ts
+++ music-app/src/app/music/home/home.component.ts
@@ -1,8 +1,10 @@
+import * as moment from 'moment';
 import { Component, OnInit } from '@angular/core';
 import { AudioService } from '../../core/services/audio.service';
 import { StateService } from '../../core/services/state.service';
 import { ITrackResponse } from '../../models/api-response.models';
 import { DeezerRestApiService } from '../../services/deezer-api.service';
+import { IGreetings } from '../../models/greeting.models';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,13 @@ import { DeezerRestApiService } from '../../services/deezer-api.service';
 })
 export class HomeComponent implements OnInit {
   trackList!: Partial<ITrackResponse>[];
+
+  greetings: IGreetings[] = [
+    { message: 'home.greeting.morning', hoursStart: 4, hoursEnd: 12 },
+    { message: 'home.greeting.afternoon', hoursStart: 12, hoursEnd: 17 },
+    { message: 'home.greeting.evening', hoursStart: 17, hoursEnd: 21 },
+    { message: 'home.greeting.night', hoursStart: 21, hoursEnd: 4 },
+  ];
 
   constructor(
     private myAudio: AudioService,
@@ -21,7 +30,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     // temporary data for player testing
     // you can remove it after first load (it will be saved in the local storage)
-    this.deezerApi.getSearch('queen', 0, 5).subscribe((response) => {
+    this.deezerApi.getSearch('queen', 0, 25).subscribe((response) => {
       this.trackList = response.data;
       this.myState.trackList$.next(response.data);
     });
@@ -38,5 +47,14 @@ export class HomeComponent implements OnInit {
 
   setTrackIndex(i: number) {
     this.myState.setPlayingTrackIndex(i);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getCurrentTimeHours() {
+    return moment().hours();
+  }
+
+  isTrackList(): boolean {
+    return Boolean(this.trackList.length);
   }
 }
