e84f155e0ba0cbe0d619fdeb3184bb02c7943e75
DashaErmolich <dasha.ermolich@gmail.com>
2023-02-26 15:22:14 +0100

    feat: add and remove track from playlist

--- music-app/src/app/music/components/custom-playlist-tracks/custom-playlist-tracks.component.ts
+++ music-app/src/app/music/components/custom-playlist-tracks/custom-playlist-tracks.component.ts
@@ -10,7 +10,19 @@ export class CustomPlaylistTracksComponent extends TracksComponent {
   @Input() customPlaylistTracks: number[] = [];
 
   addTrackToPlaylist(trackIndex: number): void {
-    this.customPlaylistTracks.push(this.tracks[trackIndex].id!);
-    console.log(trackIndex);
+    const isAdded = this.isAdded(trackIndex);
+    if (!isAdded) {
+      this.customPlaylistTracks.push(this.tracks[trackIndex].id!);
+    } else {
+      const indexInPlaylist = this.customPlaylistTracks
+        .findIndex((trackId) => trackId === this.tracks[trackIndex].id);
+      if (indexInPlaylist >= 0) {
+        this.customPlaylistTracks.splice(indexInPlaylist, 1);
+      }
+    }
+  }
+
+  isAdded(trackIndex: number): boolean {
+    return this.customPlaylistTracks.includes(this.tracks[trackIndex].id!);
   }
 }
